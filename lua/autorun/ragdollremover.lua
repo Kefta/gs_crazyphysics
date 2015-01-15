c9dd5f00c52678126dad4ee3849df981b11d3cd7
code_gs <collinharris509@icloud.com>
2015-01-14 21:45:39 -0600

    Major script overhaul
    - All spacing now unified to tab 4
    - Fixed problem with the entity being removed twice, thus braking the identify function
    - Moved all echoing into the main hook
    - Adjusted default timer and velocity values
    - Redid the IdentifyCorpse function to work in all situations; much more efficient now
    - Reduced call of freeze function to only once
    - Made freeze function much more efficient and effective
    - Deleted useless freeze run function
    - Removed bodyfound as a local file variable

--- lua/autorun/ragdollremover.lua
+++ lua/autorun/ragdollremover.lua
@@ -1,85 +1,67 @@
--- Ragdoll Crash Catcher for TTT and any other gamemode
+-- Ragdoll Crash Catcher for all gamemodes
 -- By Ambro, DarthTealc, TheEMP, and code_gs
--- Run shared (lua/autorun/)
+-- https://github.com/Kefta/Ragdoll-Remover
+-- http://facepunch.com/showthread.php?t=1347114
+-- Run shared (lua/autorun/ragdollremover.lua)
 
 -- Config
-local EchoFreeze = true 	-- Tell players when a body is frozen
-local EchoRemove = true 	-- Tell players when a body is removed
+local EchoFreeze	= true		-- Tell players when a body is frozen
+local EchoRemove	= true		-- Tell players when a body is removed
 
-local FreezeSpeed = 500 	-- Velocity ragdoll is frozen at
-local RemoveSpeed = 4000 	-- Velocity ragdoll is removed at
+local FreezeSpeed	= 2000		-- Velocity ragdoll is frozen at; this should always be less than RemoveSpeed
+local RemoveSpeed	= 4000		-- Velocity ragdoll is removed at; this should always be greater than FreezeSpeed
 
-local FreezeTime = 3 		-- Time body is frozen for
+local FreezeTime	= 1			-- Time body is frozen for
 -- End config
 
-local IsTTT = false 		-- Change to true if the name of your TTT folder is not terrortown, otherwise, leave it false
-local bodyfound = false
+local IsTTT 		= false		-- Change to true manually if the name of your TTT folder is not terrortown
 
 hook.Add( "Initialize", "GS_CheckTTT", function()
-	if ( GAMEMODE_NAME == "terrortown" ) then 
-		IsTTT = true 
-		if ( IsValid( GetConVar( "ttt_announce_body_found" ) ) ) then
-            bodyfound = GetConVar( "ttt_announce_body_found" ):GetBool() or true
-        else
-            bodyfound = true
-        end
+	if ( GAMEMODE_NAME == "terrortown" ) then
+		IsTTT = true
 	end
 end )
-
-local function SetSubPhysMotionEnabled( ent, enable )
-    if ( not IsValid( ent ) ) then return end
-	
-    if ( not enable ) then
-        ent:SetColor( Color( 255, 0, 255, 255 ) )
-        if ( IsTTT and IsValid( ent:GetOwner() ) ) then
-            ent:GetOwner():GetWeapon( "weapon_zm_carry" ):Reset( false )
-        end
-    else
-        ent:SetColor( Color( 255, 255, 255, 255 ) )
-    end
-	
-    for i = 0, ent:GetPhysicsObjectCount() - 1 do
-        local subphys = ent:GetPhysicsObjectNum( i )
-        if ( IsValid( subphys ) ) then
-            subphys:EnableMotion( enable )
-            if ( not enable ) then
-                subphys:SetVelocity( vector_origin )
-                subphys:SetMass( subphys:GetMass() * 20 )
-            else
-                subphys:SetMass( subphys:GetMass() / 20 )
-                subphys:Wake()
-            end
-        end
-	end
-
-    ent:SetVelocity( vector_origin )
-    ent:SetLocalAngularVelocity( angle_zero )
-    
-end
 
 local function KillVelocity( ent )
 	if ( not IsValid( ent ) ) then return end
 	
-	SetSubPhysMotionEnabled( ent, false )
+	ent:SetColor( Color( 255, 0, 255, 255 ) )
 	
-	if ( EchoFreeze ) then 
-		PrintMessage( HUD_PRINTTALK, "[GS_CRASH] Body frozen for " .. FreezeTime .. " seconds!" ) 
+	ent:SetVelocity( vector_origin )
+	
+	if ( IsTTT and IsValid( ent:GetOwner() ) ) then
+		ent:GetOwner():GetWeapon( "weapon_zm_carry" ):Reset( false )
 	end
 	
-	timer.Simple( FreezeTime, function() 
-		SetSubPhysMotionEnabled( ent, true )
-		if ( EchoFreeze ) then 
-			PrintMessage( HUD_PRINTTALK, "[GS_CRASH] Body unfrozen!" )
+	for i = 0, ent:GetPhysicsObjectCount() - 1 do
+		local subphys = ent:GetPhysicsObjectNum( i )
+		if ( IsValid( subphys ) ) then
+			subphys:EnableMotion( false )
+			subphys:SetMass( subphys:GetMass() * 20 )
+			subphys:SetVelocity( vector_origin )
+		end
+	end
+	
+	timer.Simple( FreezeTime, function()
+		ent:SetColor( Color( 255, 255, 255, 255 ) )
+		
+		for i = 0, ent:GetPhysicsObjectCount() - 1 do
+			local subphys = ent:GetPhysicsObjectNum( i )
+			if ( IsValid( subphys ) ) then
+				subphys:SetMass( subphys:GetMass() / 20 )
+				subphys:EnableMotion( true )
+				subphys:Wake()
+			end
 		end
 	end )
 end
 
 local function IdentifyCorpse( ent )
-    if ( not IsValid( ent ) or not CORPSE or CORPSE.GetFound( ent, false ) ) then return end
-        
+	if ( not IsValid( ent ) or not CORPSE or CORPSE.GetFound( ent, false ) ) then return end
+	
 	local dti = CORPSE.dti
 	local ply = ent:GetDTEntity( dti.ENT_PLAYER ) or player.GetByUniqueID( ent.uqid )
-	local nick = CORPSE.GetPlayerNick( ent, nil ) or ply:Nick() or "N/A" 
+	local nick = CORPSE.GetPlayerNick( ent, nil ) or ply:Nick() or "N/A"
 	local role = ent.was_role or ply:GetRole()
 	
 	if ( IsValid( ply ) ) then
@@ -89,8 +71,15 @@ local function IdentifyCorpse( ent )
 		end
 	end
 	
+	local bodyfound = true
+	
+	if ( IsValid( GetConVar( "ttt_announce_body_found" ) ) ) then
+		bodyfound = GetConVar( "ttt_announce_body_found" ):GetBool()
+	end
+	
+	local roletext
+	
 	if ( bodyfound ) then
-		local roletext = nil
 		if ( role == ROLE_TRAITOR ) then
 			roletext = "body_found_t"
 		elseif ( role == ROLE_DETECTIVE ) then
@@ -98,45 +87,48 @@ local function IdentifyCorpse( ent )
 		elseif ( role == ROLE_INNOCENT ) then
 			roletext = "body_found_i"
 		end
-
-		LANG.Msg( "body_found", { finder = "The Server",
-		victim = nick,
-		role = LANG.Param( roletext ) } )
+		
+		if ( roletext ) then
+			LANG.Msg( "body_found", { finder = "The Server", victim = nick, role = LANG.Param( roletext ) } )
+		end
 	end
-
+	
 	CORPSE.SetFound( ent, true )
 	
-	for k, vicid in pairs( ent.kills ) do
-		
+	for _, vicid in pairs( ent.kills ) do
 		local vic = player.GetByUniqueID( vicid )
-		
 		if ( IsValid( vic ) and not vic:GetNWBool( "body_found", false ) ) then
-			LANG.Msg( "body_confirm", { finder = "The Server", victim = vic:Nick() } )
+			LANG.Msg( "body_confirm", { finder = "The Server", victim = vic:Nick() or "N/A" } )
 			vic:SetNWBool( "body_found", true )
 		end
 	end
 end
 
 function GS_CrashCatch()
-    for k, ent in pairs( ents.FindByClass( "prop_ragdoll" ) ) do
-        if ( IsValid( ent ) and ent.player_ragdoll ) then
-            local velo = ent:GetVelocity():Length()
-			local nick = ent:GetNWString( "nick", "N/A" )
-            if ( velo >= RemoveSpeed ) then
-                if ( IsTTT ) then
-                    IdentifyCorpse( ent )
-                end
-                ent:Remove()
-                local message = "[GS_CRASH] Removed body of " .. nick .. " for moving too fast"
-                ServerLog( message .. " (" .. velo .. ")\n" )
-                if ( EchoRemove ) then
+	for _, ent in pairs( ents.FindByClass( "prop_ragdoll" ) ) do
+		if ( IsValid( ent ) and ent.player_ragdoll ) then
+			local velo = ent:GetVelocity():Length()
+			if ( velo >= RemoveSpeed ) then
+				if ( IsTTT ) then
+					IdentifyCorpse( ent )
+				end
+				ent:Remove()
+				local nick = ent:GetNWString( "nick", "N/A" )
+				local message = "[GS_CRASH] Removed body of " .. nick .. " for moving too fast"
+				ServerLog( message .. " (" .. velo .. ")\n" )
+				if ( EchoRemove ) then
 					PrintMessage( HUD_PRINTTALK, message )
 				end
-            elseif ( velo >= FreezeSpeed ) then
-                KillVelocity( ent )
-                ServerLog( "[GS_CRASH] Disabling motion for the body of " .. nick .. " (" .. velo .. ") \n" )
-            end
-        end
-    end
+			elseif ( velo >= FreezeSpeed ) then
+				KillVelocity( ent )
+				local nick = ent:GetNWString( "nick", "N/A" )
+				local message = "[GS_CRASH] Froze body of " .. nick .. " for moving too fast"
+				ServerLog( message .. " (" .. velo .. ") \n" )
+				if ( EchoFreeze ) then
+					PrintMessage( HUD_PRINTTALK, message )
+				end
+			end
+		end
+	end
 end
 hook.Add( "Think", "GS_CrashCatcher", GS_CrashCatch )
