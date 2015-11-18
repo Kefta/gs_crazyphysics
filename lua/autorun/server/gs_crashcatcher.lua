2659b425b2328d77bd8798aa291d8d6621a761f2
code_gs <collinharris509@icloud.com>
2015-11-17 22:26:17 -0600

    Updated comments

--- lua/autorun/server/gs_crashcatcher.lua
+++ lua/autorun/server/gs_crashcatcher.lua
@@ -1,6 +1,8 @@
--- Entity crash prevention
--- By Ambro, DarthTealc, TheEMP, and code_gs
--- http://facepunch.com/showthread.php?t=1347114
+--Entity Crash Catcher
+--Entity crash catcher for all gamemodes. Detects entities that are either going to fast or have invalid positions, and stops them from crashing the server.
+-- By code_gs, Ambro, DarthTealc, TheEMP
+-- Facepunch thread: http://facepunch.com/showthread.php?t=1347114
+-- This script is also implemented into pLib: https://github.com/SuperiorServers/plib_v2
 
 -- Config
 local EchoFreeze	= false		-- Tell players when a body is frozen
